9cd9c0173723601811904f2b05fc47d0c690a9b2
Maximilian Müller <maximilian-mueller@gmx.at>
2017-11-29 15:53:58 +0100

    minor change

--- src/client.js
+++ src/client.js
@@ -173,7 +173,8 @@ Client.prototype.createDiff = function(docA, docB){
  * @param  {Diff} patch
  */
 Client.prototype.applyPatchTo = function(obj, patch){
-  this.jsondiffpatch.patch(obj, patch);
+    this.emit('diff', editMessage.diff)
+    this.jsondiffpatch.patch(obj, patch);
 };
 
 /**
@@ -251,7 +252,6 @@ Client.prototype.applyServerEdit =  function(editMessage){
 
     if(!isEmpty(editMessage.diff)){
 
-      this.emit('diff', editMessage.diff)
       // versions match
       // 3) patch the shadow
       this.applyPatchTo(this.doc.shadow, editMessage.diff);
